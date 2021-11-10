197c81172b4771eb0a5c93e2b0ef157875f0eb72
Mann-tech13 <mannshah.ict19@gmail.com>
2021-11-10 10:34:17 +0530

    .

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -2,28 +2,38 @@ import React from 'react';
 import Header from "./Header"
 import Sidebar from "./Sidebar"
 import RecommendedVideos from "./RecommendedVideos"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import './App.css';
+import SearchPage from "./SearchPage"
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">  
 
-      {/* ------------------------------------------------------- */}
-      {/* -------------- Header ---------------- */}
-      <Header/>
-      {/* ------------------------------------------------------- */}
-
-
-      {/* ------------------------------------------------------- */}
-      {/* -------------- App page ---------------- */}
+    <Router>
+      <Switch>
+        
+        <Route path="/search/:searchTerm">
+          <Header/>
+          <div className="app_page">
+            <Sidebar/>
+            <SearchPage/>
+          </div>
+        </Route>
+        <Route path="/">
+          <Header/>
+          <div className="app_page">
+            <Sidebar/>
+            <RecommendedVideos/>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
+    {/* <Header/>
       <div className="app_page">
-
-        {/* ------ Sidebar ------ */}
         <Sidebar/>
-
-        {/* ------ Videos ------- */}
         <RecommendedVideos/>
-      </div>
+      </div> */}
     </div>
   );
 }
--- youtube-clone/src/Header.js
+++ youtube-clone/src/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Header.css"
 import MenuIcon from "@material-ui/icons/Menu"
 import SearchIcon from "@material-ui/icons/Search"
@@ -7,24 +7,37 @@ import AppsIcon from "@material-ui/icons/Apps"
 import NotificationsIcon from "@material-ui/icons/Notifications"
 import Avatar from "@material-ui/core/Avatar"
 import logo from "./YT-logo.svg"
+import { Link } from "react-router-dom"
 
 function Header() {
+
+  const [inputSearch, setInputSearch] = useState("")
+
   return (
     <div className="header">
       <div className="header_left">
         <MenuIcon/>
+
+        {/* <Link to="/">
+          <img className="header_logo" src="./YT-logo.svg" alt="" />
+        </Link> */}
+
         <img className="header_logo" src={logo} alt="YT"/>
       </div>
       <div className="header_input">
-        <input type="text"/>
-        <SearchIcon className="header_inputButton"/>
+        <input onChange={ (e) => setInputSearch(e.target.value) } value={ inputSearch } placeholder="Search" type="text"/>
+
+        <Link to={ `/search/${inputSearch}` }>
+          <SearchIcon className="header_inputButton"/>
+        </Link>
+        
       </div>
       
       <div className="header_icons">
-        <VideoCallIcon/>
-        <AppsIcon/>
-        <NotificationsIcon/>
-        <Avatar/>
+        <VideoCallIcon className="vc"/>
+        <AppsIcon className="ai"/>
+        <NotificationsIcon className="ni"/>
+        <Avatar className="av"/>
       </div>
     </div>
   )
